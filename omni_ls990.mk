0fb73048c8db8df834c4109a7492968d2776901d
Aleksander <johnnylittleplanet@gmail.com>
2018-06-30 13:46:30 +0200

    add .dependencies and remove device.mk call

--- omni_ls990.mk
+++ omni_ls990.mk
@@ -4,9 +4,6 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from ls990 device
-$(call inherit-product, device/lge/ls990/device.mk)
-
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := ls990
 PRODUCT_NAME := omni_ls990
